3171c79986b2fa516a001fcbc5f2da9bdf1a7bd9
anukulsinghal <anukul.singhal@gmail.com>
2016-05-11 12:51:24 +0530

    removing unnecessary files from runManager

--- runManager/pytest_driver.py
+++
@@ -1,9 +0,0 @@
-import pytest
-
-'''
-    Executing Selenium Grid tests in distributed mode, with use of pytest-xdist.
-    Need to define processes to be opened by popen.  Currently, 1 process per test on 
-    each remote machine looks good.
-'''
-pytest_args = "--tx 1*popen --dist=each SeleniumGrid_Distributed_Driver.py -q --tb=line"
-pytest.main(pytest_args)
